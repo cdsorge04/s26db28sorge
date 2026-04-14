6a4ed8cd47c062b66842dfe3cf7509518ea44aa1
cdsorge04 <cdsorge04@gmail.com>
2026-04-13 19:07:26 -0500

    assignment 12 done

--- controllers/chips.js
+++ controllers/chips.js
@@ -116,3 +116,18 @@ exports.chips_update_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+// Handle a delete one view with id from query
+exports.chips_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        let result = await chips.findById(req.query.id)
+        res.render('chipsdelete', { 
+            title: 'Chips Delete', 
+            toShow: result 
+        });
+    }
+    catch(err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/chips.js
+++ routes/chips.js
@@ -8,4 +8,5 @@ router.delete('/chips/:id', chips_controller.chips_delete);
 router.get('/detail', chips_controller.chips_view_one_Page);
 router.get('/create', chips_controller.chips_create_Page);
 router.get('/update', chips_controller.chips_update_Page);
+router.get('/delete', chips_controller.chips_delete_Page);
 module.exports = router;
